2012a633e641cf02d0ca5feaa98e4dd27175fb39
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-06 13:28:47 -0600

    DISABLE_EVENT_STORE_LEADER_DETECTION env var is set in test init

--- test/test_init.rb
+++ test/test_init.rb
@@ -3,6 +3,8 @@ ENV['LOG_LEVEL'] ||= '_min'
 
 ENV['ENTITY_CACHE_SCOPE'] ||= 'exclusive'
 
+ENV['DISABLE_EVENT_STORE_LEADER_DETECTION'] ||= 'on'
+
 require_relative '../init.rb'
 
 require 'data_aggregation/accumulator/controls'
